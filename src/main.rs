0961c85772a029b2f29ec34a740d35eabb9ff41e
nituc10 <55444261+nituc10@users.noreply.github.com>
2023-07-22 15:10:27 -0400

    Variables, functions and print line macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+fn add(num_one: i32, num_two: i32) -> i32 {
+    // explicitly type annotate the parameters and value returned
+    // we can drop the return keyword when the last line of the function is an expression
+    num_one + num_two
+}
+
 fn main() {
-    println!("Hello, world!");
+    let foo = add(10, 5);
+    // using the print line macros
+    // '!' always appears after macro names, this is how you identify a macro
+
+    // compiler throws errors if you don't pass a string as argument for print line macro, hence we use formatting
+    println!("{}", foo);
+
+    // output multiple values
+    println!("{} {}", foo, false);
+
+    // supports positional arguments
+    // number represents the index of the argument in the argument list
+    println!("{0} {0}", foo);
+
+    // complex values
+    println!("{:?}", (12, true, "hello"));
 }
